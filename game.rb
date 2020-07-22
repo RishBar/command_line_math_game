733ba6558166056f51d2ecd123fb3fca48aca3db
Rishav Baral <rishavbaral6@gmail.com>
2020-07-21 21:41:44 -0700

    decrement player.score if question.true_false is false, then while loop will stop running when player.score is less then 1

--- game.rb
+++ game.rb
@@ -8,7 +8,7 @@ end
 
 class Question
   attr_accessor :true_false
-  def initialize()
+  def initialize
     num_one = rand(20)
     num_two = rand(20)
     puts "#{num_one} + #{num_two} = ?"
@@ -29,18 +29,32 @@ class Game
     @player_one = Player.new(gets.chomp)
     puts 'Player 2 name?'
     @player_two = Player.new(gets.chomp)
+    puts '----------start game-----------'
   end
 
-  def start_game()
+  def start_game
     counter = 0
-    while player_one.score.positive? && player_two.positive?
+    while player_one.score.positive? && player_two.score.positive?
       player = counter % 2 == 0? @player_one : @player_two
+      other_player = counter % 2 == 1? @player_one : @player_two
       puts "Question for #{player.name}"
       question = Question.new
-      counter += 1
+      if !question.true_false
+        player.score += -1
+      end
+      if player.score.positive?
+        puts "#{player.name} has a score of #{player.score}/3"
+        puts "#{other_player.name} has a score of #{other_player.score}/3"
+        puts '-----------new turn-----------------'
+        counter += 1
+      else
+        puts "#{player.name} loses\n#{other_player.name} wins with a score of #{other_player.score}/3"
+        puts '---------GAME OVER-----------'
+        puts 'Goodbye!'
+      end
     end
   end
 end
 
-p = Game.new()
+p = Game.new
 p.start_game
