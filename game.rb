756b59f35d5605914c8733eb0dd238acf9401afd
Rishav Baral <rishavbaral6@gmail.com>
2020-07-21 21:17:19 -0700

    add class for questions and add method in Game class called start_game that will do a while loop and keep asking questions while player1.score and player2.score are both positive

--- game.rb
+++ game.rb
@@ -6,12 +6,41 @@ class Player
   end
 end
 
+class Question
+  attr_accessor :true_false
+  def initialize()
+    num_one = rand(20)
+    num_two = rand(20)
+    puts "#{num_one} + #{num_two} = ?"
+    if gets.chomp.to_i == num_one + num_two
+      puts 'Correct'
+      @true_false = true
+    else
+      puts 'Wrong'
+      @true_false = false
+    end
+  end
+end
+
 class Game
-  attr_accessor :name
+  attr_accessor :player_one, :player_two
   def initialize()
     puts 'Player 1 name?'
-    player_one = Player.new(gets.chomp)
+    @player_one = Player.new(gets.chomp)
+    puts 'Player 2 name?'
+    @player_two = Player.new(gets.chomp)
+  end
+
+  def start_game()
+    counter = 0
+    while player_one.score.positive? && player_two.positive?
+      player = counter % 2 == 0? @player_one : @player_two
+      puts "Question for #{player.name}"
+      question = Question.new
+      counter += 1
+    end
   end
 end
 
 p = Game.new()
+p.start_game
